c2a29af45cb1f25facc6a58465a296f9b9b56edf
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-09-06 09:56:21 +0200

    Initial commit.

---
+++ django_acdhch_functions/views.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.views.generic import TemplateView
+
+from . import utils
+
+
+class Imprint(TemplateView):
+    template_name = "acdhch_functions/imprint.html"
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["imprint"] = utils.get_impressum()
+        ctx["basetemplate"] = getattr(settings, "BASE_TEMPLATE", "base.html")
+        return ctx
